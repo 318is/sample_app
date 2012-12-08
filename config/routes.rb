ad574100921221bdf5f3d14bb91733884ae5a074
318is <robertbalogh1986@gmail.com>
2012-12-08 19:51:38 +0100

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
-	resources :users
-  	resources :sessions, only: [:new, :create, :destroy]
-  	
-  	root to: 'static_pages#home'
 
-    match '/help',    to: 'static_pages#help'
-    match '/about',   to: 'static_pages#about'
-    match '/contact', to: 'static_pages#contact'
-	match '/signup',  to: 'users#new'
-  	match '/signin',  to: 'sessions#new'
-  	match '/signout', to: 'sessions#destroy', via: :delete
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 	
 
   # The priority is based upon order of creation:
